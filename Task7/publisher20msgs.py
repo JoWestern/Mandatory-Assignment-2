4ffccb6ccd8dc3df1b994ba5f45e41690991c825
joachimwestern <joawes@norkart.no>
2024-04-01 18:11:44 +0200

    created folder structure

---
+++ Task7/publisher20msgs.py
@@ -0,0 +1,53 @@
+import random
+import time
+from paho.mqtt import client as mqtt_client
+
+broker = 'broker.emqx.io'
+port = 1883
+topic = "Sensor/Temp"
+client_id = f'publish-{random.randint(0, 1000)}'
+# username = 'emqx'
+# password = 'public'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Publisher connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    
+    def on_connack(client, userdata, flags, rc):
+        print("Connack received with code", rc)
+
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    # client.on_connect = on_connack
+    client.connect(broker, port)
+    return client
+
+def publish(client):
+    msg_count = 1
+    while True:
+        time.sleep(1)
+        msg = f"Værmelding: {msg_count}"
+        result = client.publish(topic, msg, qos=1)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+        if msg_count > 20:
+            break
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+    client.loop_stop()
+
+
+if __name__ == '__main__':
+    run()
